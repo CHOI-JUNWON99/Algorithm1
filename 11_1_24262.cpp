97d66986ec1ac8cd6b747a9fe4918653b3b3a4b1
61962059 <79883621+61962059@users.noreply.github.com>
2024-02-18 16:53:57 +0900

    Add files via upload

---
+++ 11_1_24262.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	cout << 1 << endl << 0;
+
+}
+/*
+class MenOfPassion(A[], n) {
+	i = [n / 2];
+	return A[i];
+}
+*/
