374746100e54edf548e73f5c0f0db3f348abf2df
CHOI-JUNWON99 <79883621+CHOI-JUNWON99@users.noreply.github.com>
2024-02-18 18:11:22 +0900

    Add files via upload

---
+++ 11_2_24263.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    cout << n << endl << "1";
+
+}
+
+/*
+MenOfPassion(A[], n) {
+    sum <- 0;
+    for i <- 1 to n
+        sum <- sum + A[i]; # ??1
+    return sum;
+}
+*/
