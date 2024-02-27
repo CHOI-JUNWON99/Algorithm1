b438feb84848eb442a708ee31820dab923e0f6a7
CHOI-JUNWON99 <79883621+CHOI-JUNWON99@users.noreply.github.com>
2024-02-27 22:52:21 +0800

    Add files via upload

---
+++ 12_5_1436.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int find(int N) {
+	
+	int i = 666;
+	string target;
+	int count = 0;
+	while (1) {
+		target = to_string(i);
+		for (int j = 0; j < target.length()-2; j++) { //j<target.length()-2를 생각하기가 오래걸렸다.
+			if (target[j] == '6' && target[j + 1] == '6' && target[j + 2] == '6') {
+				count++;
+				if (count == N)
+					return i;
+				break;				
+			}
+		}
+		i++;
+	}
+}
+
+int main() {
+	int N;
+	cin >> N;
+	cout << find(N);
+}
