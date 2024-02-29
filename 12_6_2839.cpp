989de152d24419ade46084a5afbf11b4e09da557
CHOI-JUNWON99 <79883621+CHOI-JUNWON99@users.noreply.github.com>
2024-02-29 15:54:43 +0800

    Add files via upload

---
+++ 12_6_2839.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	int a = N / 5;
+	int b = 0;
+	
+	while (1) {
+		if (a < 0) {
+			cout << "-1";
+			return 0;
+		}
+		if ((N - (5 * a)) % 3 == 0) {
+			b = ((N - (5 * a)) / 3);
+			break;
+		}
+		a--;
+	}
+	cout << a + b;
+}
